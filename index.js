11095d87ae0581953feabdbfd3d213733d3736d7
Mbalenhle <mbalindaba01@gmail.com>
2021-12-06 22:59:25 +0200

    added screen for shop list

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 const express = require('express');
 const exphbs  = require('express-handlebars');
+const avos = require('./avo-shopper')
+const pg = require("pg");
+const Pool = pg.Pool;
+require('dotenv').config()
 
 const app = express();
 const PORT =  process.env.PORT || 3019;
+
+//connect to database 
+const connectionString = process.env.DATABASE_URL || 'postgresql://postgres:Minenhle!28@localhost:5432/avo_shopper';
+
+const pool = new Pool({
+	connectionString
+});
+
+const Avos = avos(pool)
+
 
 // enable the req.body object - to allow us to use HTML forms
 app.use(express.json());
@@ -23,6 +37,13 @@ app.get('/', function(req, res) {
 		counter
 	});
 });
+
+app.post('/shops', async function (req, res){
+	let shopList = await Avos.listShops()
+	res.render('shoplist', {
+		list: shopList
+	})
+})
 
 // start  the server and start listening for HTTP request on the PORT number specified...
 app.listen(PORT, function() {
